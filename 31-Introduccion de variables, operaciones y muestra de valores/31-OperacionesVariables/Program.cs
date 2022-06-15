039f38fdd43fd007513cc4fc8d29ddca6c7fc12c
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:44:36 -0300

    Add files via upload

---
+++ 31-Introduccion de variables, operaciones y muestra de valores/31-OperacionesVariables/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _31_OperacionesVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+
+            double num2;
+
+            char letra = 'A';
+            
+
+            num1 = 29;
+            num2 = 12.2;
+
+            double suma = num1 + num2;
+
+            double resta = num2 - num1;
+
+            Console.WriteLine("Se han ingresado los valores: " + num1 + " y " + num2 + " se los ha sumado y restado");
+
+            Console.WriteLine("El resultado de la suma de es: " + suma + Environment.NewLine + "El resultado de la resta es: " + resta);
+
+            Console.WriteLine("Tambien se ha ingresado una variable de tipo CHAR cuyo valor es: " + letra);
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
